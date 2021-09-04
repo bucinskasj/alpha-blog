b78f00025f2ddb1e6ac835aafa0d526557015744
Bucinskas <jonas@bucinskas.lt>
2021-09-04 08:44:24 +0300

    Adding validation for articles model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-
+  validates :title, presence: true, length: {minimum: 6, masimum: 100}
+  validates :description, presence: true, length: {minimum: 10, masimum: 300}
 end
